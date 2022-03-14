19d8afe1d8f6e84490c224b18ef21fb31ec8710c
Pranaybhavsar <86315266+Pranaybhavsar@users.noreply.github.com>
2022-03-14 17:32:27 +0530

    Create main.py

---
+++ OTAUPDATES/main.py
@@ -0,0 +1,12 @@
+import time
+from machine import Pin
+led1=Pin(4,Pin.OUT)
+led2=Pin(5,Pin.OUT)#create LED object from pin2,Set Pin2 to output
+
+while True:
+  led.value(1) 
+  led2.value(1)#Set led turn on
+  time.sleep(1)
+  led.value(0)
+  led2.value(0)#Set led turn off
+  time.sleep(1)
